518bae672a84488bd7df6bdfc118fffc76448e22
LiamMcC <me@liammccabe.ie>
2024-01-25 19:54:56 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var app = express();
+
+
+app.set('view engine', 'ejs'); // Set the template engine 
+
+
+app.get('/', function(req,res){
+           res.render('home')   
+  
+})
+
+app.get('/about', function(req,res){
+
+    res.render('about')
+
+
+})
+
+
+// **********************************  Code to here **************************
+
+app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0" , function(){
+  console.log("New Full Demo is Live")
+});
---
+++ readme.txt
@@ -0,0 +1,6 @@
+1. create folder
+2. Open folder in vs code
+3. Create views folder with at least 1 file in it which is .ejs
+4. Creaate a file called index.js which is the brains of the app
+5. in the terminal run npm init (press enter)
+6. answer all questions 
